6ee731e8ba9e094161214c1e18a518f917e59e85
44filip <100999946+44filip@users.noreply.github.com>
2024-03-05 02:00:36 +0100

    user interface setup

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from task import Task
 from task_manager import TaskManager
-
 task_manager = TaskManager()
 
-task_manager.add("Create game")
-task_manager.add("draw art")
-task_manager.complete("DRAw art")
-task_manager.add("Compose music")
-task_manager.remove("compose music")
-task_manager.displayTasks()
+# task_manager.add("Create game")
+# task_manager.add("draw art")
+# task_manager.complete("DRAw art")
+# task_manager.add("Compose music")
+# task_manager.remove("compose music")
+# task_manager.displayTasks()
+
+print("Welcome to PYTodo")
+while True:
+    print("\n1. Add item to list\n2. Remove item from the list\n3. Mark item as completed\n4. Display all tasks\n5. Exit")
+    res = input("Enter your choice: ")
+
+    match res:
+        case "1":
+            add_input = input("Enter title of the task to be added: ")
+            task_manager.add(add_input)
+        case "2":
+            remove_input = input("Enter title of the task to be removed: ")
+            task_manager.remove(remove_input)
+        case "3":
+            complete_input = input("Enter title of the task to be marked complete: ")
+            task_manager.complete(complete_input)
+        case "4":
+            task_manager.displayTasks()
+        case "5":
+            exit()
+        case _:
+            print("Invalid input.")
