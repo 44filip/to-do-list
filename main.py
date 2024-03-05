9424f1c6cf6da236e85b3117f87a263c01bca1ef
44filip <100999946+44filip@users.noreply.github.com>
2024-03-05 02:18:42 +0100

    Removing redundant import, setting up main()

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from task import Task
 from task_manager import TaskManager
 task_manager = TaskManager()
 
@@ -9,27 +8,31 @@ task_manager = TaskManager()
 # task_manager.remove("compose music")
 # task_manager.displayTasks()
 
-print("Welcome to PYTodo")
-while True:
-    print("\n1. Add item to list\n2. Remove item from the list\n3. Mark item as completed\n4. Display all tasks\n5. Exit\n")
-    res = input("Enter your choice: ")
+def main():
+    print("Welcome to PYTodo")
+    while True:
+        print("\n1. Add item to list\n2. Remove item from the list\n3. Mark item as completed\n4. Display all tasks\n5. Exit\n")
+        res = input("Enter your choice: ")
 
-    match res:
-        case "1":
-            add_input = input("Enter title of the task to be added: ")
-            task_manager.add(add_input)
-        case "2":
-            if task_manager.checkContent():
-                remove_input = input("Enter title of the task to be removed: ")
-                task_manager.remove(remove_input)
-        case "3":
-            if task_manager.checkContent():
-                complete_input = input("Enter title of the task to be marked complete: ")
-                task_manager.complete(complete_input)
-        case "4":
-            if task_manager.checkContent():
-                task_manager.displayTasks()
-        case "5":
-            exit()
-        case _:
-            print("Invalid input.")
+        match res:
+            case "1":
+                add_input = input("Enter title of the task to be added: ")
+                task_manager.add(add_input)
+            case "2":
+                if task_manager.checkContent():
+                    remove_input = input("Enter title of the task to be removed: ")
+                    task_manager.remove(remove_input)
+            case "3":
+                if task_manager.checkContent():
+                    complete_input = input("Enter title of the task to be marked complete: ")
+                    task_manager.complete(complete_input)
+            case "4":
+                if task_manager.checkContent():
+                    task_manager.displayTasks()
+            case "5":
+                exit()
+            case _:
+                print("Invalid input.")
+                
+if __name__ == "__main__":
+    main()
