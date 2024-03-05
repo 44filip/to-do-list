e926d8235abb0f6a392774bd794758c2921a822b
44filip <100999946+44filip@users.noreply.github.com>
2024-03-05 01:46:56 +0100

    testing prototype

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from task import Task
 from task_manager import TaskManager
 
-TaskManager()
+task_manager = TaskManager()
+
+task_manager.add("Create game")
+task_manager.add("Draw art")
+task_manager.complete("Draw art")
+task_manager.displayTasks()
--- task_manager.py
+++ task_manager.py
@@ -7,15 +7,18 @@ class TaskManager:
     def add(self, title : str):
         task = Task(title)
         self.tasks.append(task)
+        print("200 - OK")
         
     def remove(self, title : str):
         # Iterate and create a new list from each task in self.tasks that doesn't match the title
         self.tasks = [task for task in self.tasks if task.title != title]
+        print("200 - OK")
         
     def complete(self, title : str):
         for task in self.tasks:
             if task.title == title:
                 task.completed = True
+        print("200 - OK")
                 
     def displayTasks(self):
         for task in self.tasks:
