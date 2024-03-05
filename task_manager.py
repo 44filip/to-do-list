f27ea93581a4bf360992aa7ab5b93671dcc76a0c
44filip <100999946+44filip@users.noreply.github.com>
2024-03-05 01:42:57 +0100

    add, remove, complete, display

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,23 @@
+from task import Task
+
 class TaskManager:
     def __init__(self):
         self.tasks = []
+        
+    def add(self, title : str):
+        task = Task(title)
+        self.tasks.append(task)
+        
+    def remove(self, title : str):
+        # Iterate and create a new list from each task in self.tasks that doesn't match the title
+        self.tasks = [task for task in self.tasks if task.title != title]
+        
+    def complete(self, title : str):
+        for task in self.tasks:
+            if task.title == title:
+                task.completed = True
+                
+    def displayTasks(self):
+        for task in self.tasks:
+            status = "Completed" if task.completed else "Not completed"
+            print(f"{task.title} - {status}")
